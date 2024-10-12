3d2a39e6c4efc2b7eb7042723fff1ff8f0c256f2
Aaron Ramirez <ramirezja16@gmail.com>
2024-10-12 12:18:39 -0700

    Changed login import so it points to the correct filename.

--- DiverseMakersApp/app/index.tsx
+++ DiverseMakersApp/app/index.tsx
@@ -1,16 +1,15 @@
 import { Text, View, Button } from "react-native";
-import {collection, doc, setDoc} from 'firebase/firestore/lite';
-import { PaperProvider } from 'react-native-paper';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
-import * as React from 'react';
-import Login from "./Login";
+import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { PaperProvider } from "react-native-paper";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from "react";
+import Login from "./login";
 import { FIREBASE_APP, FIREBASE_AUTH, FIREBASE_DB } from "./firebaseConfig";
 
 const Stack = createNativeStackNavigator();
 
 export default function Index() {
   return (
-    
     <PaperProvider>
       {/* <View
         style={{
@@ -26,33 +25,32 @@ export default function Index() {
               color="#841584"
             />
       </View> */}
-              <Button
-              onPress={() => addUser( "TestUser2", "TestUsername2")}
-              title="Get Users"
-              color="#841584"
-            />
-        <Stack.Navigator initialRouteName="login">
-          <Stack.Screen
-          name = "login"
-          component = {Login}
-          options={{headerShown: false}}/>
-        </Stack.Navigator>
+      <Button
+        onPress={() => addUser("TestUser2", "TestUsername2")}
+        title="Get Users"
+        color="#841584"
+      />
+      <Stack.Navigator initialRouteName="login">
+        <Stack.Screen
+          name="login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
     </PaperProvider>
-    
   );
 }
 
 async function getUsers() {
-  const usersCol = collection(FIREBASE_DB, 'users'); 
-  //const userSnapshot = await getDocs(usersCol); 
+  const usersCol = collection(FIREBASE_DB, "users");
+  //const userSnapshot = await getDocs(usersCol);
   console.log(usersCol);
-  
 }
 
 async function addUser(userId: string, usernameToAdd: string) {
   // create a user in users collection with TestUser doc ID
-  await setDoc(doc(FIREBASE_DB, 'users', userId), {
+  await setDoc(doc(FIREBASE_DB, "users", userId), {
     // set username to test username
-    username: usernameToAdd
+    username: usernameToAdd,
   });
 }
