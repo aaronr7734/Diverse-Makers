45fac5dc995309563d518ca8b99452456fc3370e
Kane Davidson <kaned2001@icloud.com>
2024-10-16 21:50:10 -0700

    Updated tab navigation to include icons.

--- DiverseMakersApp/app/(tabs)/_layout.tsx
+++ DiverseMakersApp/app/(tabs)/_layout.tsx
@@ -1,10 +1,32 @@
 import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons'; // Import the icon set you want to use
+import React from 'react';
 
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Home Screen', headerShown: false}} />
-      <Tabs.Screen name="profileContainer" options={{ title: 'Profile' }} /> 
+      {/* Home Tab */}
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home Screen',
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" color={color} size={size} />
+          ),
+        }}
+      />
+
+      {/* Profile Tab */}
+      <Tabs.Screen
+        name="profileContainer"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" color={color} size={size} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
